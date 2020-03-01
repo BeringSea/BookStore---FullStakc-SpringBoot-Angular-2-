99fb97a5223608fc6a4ec23e8df7d4e23578f43a
David Mojicevic <david.mojicevic@eng.it>
2020-03-01 17:10:13 +0100

    Book store FullStack (SpringBoot + Angular 2+) application

---
+++ Backend/bookstore-angular/src/main/java/com/bookstore/repository/BillingAddressRepository.java
@@ -0,0 +1,7 @@
+package com.bookstore.repository;
+
+import com.bookstore.domain.BillingAddress;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BillingAddressRepository extends CrudRepository<BillingAddress, Long> {
+}
